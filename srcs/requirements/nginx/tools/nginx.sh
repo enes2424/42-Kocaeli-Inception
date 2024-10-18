e08c10ea5e41b643239b464c56c50d4b195e64b1
enes2424 <132240375+enes2424@users.noreply.github.com>
2024-10-18 06:56:49 -0500

    Add files via upload

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+openssl req -x509 \
+			-nodes \
+			-days 365 \
+			-newkey rsa:4096 \
+			-keyout $PRIVATE_KEY \
+			-out $CERTIFICATE \
+			-subj "/CN=$DOMAIN_NAME";
+
+sed -i "s|!DOMAIN_NAME!|$DOMAIN_NAME|g" /etc/nginx/sites-enabled/default
+sed -i "s|!CERTIFICATE!|$CERTIFICATE|g" /etc/nginx/sites-enabled/default
+sed -i "s|!PRIVATE_KEY!|$PRIVATE_KEY|g" /etc/nginx/sites-enabled/default
+
+exec "$@"
---
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Web sunucusu ve PHP çalıştırma izinlerini ayarla
+chown -R www-data:www-data /var/www/*
+chmod -R 755 /var/www/*
+mkdir -p /run/php/
+chown -R www-data:www-data /run/php
+touch /run/php/php7.4-fpm.pid
+
+# Eğer wp-config.php dosyası yoksa WordPress'i kur
+if [ ! -f /var/www/html/wp-config.php ]; then
+    rm -rf /var/www/html/*
+
+    # WordPress çekirdeğini indir
+    wp core download --allow-root
+
+    # WordPress yapılandırma dosyasını oluştur
+    wp config create --allow-root \
+        --dbname=${MYSQL_DATABASE} \
+        --dbuser=${MYSQL_USER} \
+        --dbpass=${MYSQL_PASSWORD} \
+        --dbhost=${WORDPRESS_DB_HOST:-mariadb} \
+        --skip-check
+
+    # WordPress'i kur
+    wp core install --allow-root \
+        --url=${DOMAIN_NAME} \
+        --title="eates" \
+        --admin_user=${WORDPRESS_ADMIN_USER} \
+        --admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+        --admin_email=${WORDPRESS_ADMIN_EMAIL}
+
+    # WordPress kullanıcı hesabı oluştur
+    wp user create --allow-root \
+        ${WORDPRESS_USER} \
+        ${WORDPRESS_EMAIL} \
+        --user_pass=${WORDPRESS_PASSWORD}
+fi
+
+# PHP-FPM başlat
+exec "$@"
